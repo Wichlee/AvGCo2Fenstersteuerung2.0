5d4c0be355421c8b740662a7c594a41a9fee6475
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2022-10-12 14:26:06 +0200

    control_unit.py empfängt die Messungen auf topic "AvG/co2level". Es kann aber noch keine Information verarbeitet oder weiterversendet werden an das topic des window_actor.py

--- control_unit.py
+++ control_unit.py
@@ -2,20 +2,22 @@ from random import randint
 import time
 
 from paho.mqtt import client as mqtt_client
+from paho.mqtt.client import Client
 
-#set broker parameters and topic
+# set broker parameters and topic
 broker = 'test.mosquitto.org'
 port = 1883
 topic_co2level = "AvG/co2level"
-topic_actwindow = "AvG/actwindow"
+topic_actorwindow = "AvG/actorwindow"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{randint(0, 1000)}'
+
 
 # connect routine
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to MQTT Broker", broker, port)
+            print("Controller connected to MQTT Broker", broker, port)
         else:
             print("Failed to connect, return code %d\n", rc)
 
@@ -24,34 +26,36 @@ def connect_mqtt():
     client.connect(broker, port)
     return client
 
+
 # publish routine
 def publish_window_actor(client):
-
     while True:
         time.sleep(1)
-        randNumber = int(randint(1000, 3500))
-        result = client.publish(topic, randNumber)
+        subscribe(client)
+        result = client.publish(topic_actorwindow, subscribe(client))
         # result: [0, 1]
         status = result[0]
         if status == 0:
-            print(f"Send `{randNumber}` to topic `{topic}`")
+            print(f"Send `{subscribe(client)}` to topic `{topic_actorwindow}`")
         else:
-            print(f"Failed to send message to topic {topic}")
+            print(f"Failed to send message to topic {topic_actorwindow}")
         time.sleep(10)
 
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        # return msg.payload.decode()
 
-    client.subscribe(topic)
+    client.subscribe(topic_co2level)
     client.on_message = on_message
 
 
 def run():
     client = connect_mqtt()
-    client.loop_start()
-    publish_window_actor(client)
+    subscribe(client)
+    # publish_window_actor(client)
+    client.loop_forever()
 
 
 if __name__ == '__main__':
